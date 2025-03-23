b6f68fbfd85e2f3a07183c0dcab9fe16dfa48fe0
Thomas <thomaschow0716@gmail.com>
2025-03-23 12:57:50 +0000

    Added read function and checks for primary keys, prevent update duplication

--- main.py
+++ main.py
@@ -19,48 +19,88 @@ tickers = ["AAPL", "NVDA", "MSFT", "AMZN", "META", "GOOGL", "AVGO", "TSLA", "BRK
  "JNJ", "HD", "ABBV", "BAC", "CRM"]
 
 
-def readData(table_id, rows_list):
+def writeData(table_id, rows_list):
     errors = client.insert_rows_json(table_id, rows_list)
+
     if errors == []:
         print(f'{len(rows_list)} Rows added.')
     else:
         print(f'Error: {errors}')
 
-def update(tickers):
-    company_rows = []
+def readData(table_id, columns):
+    unique_keys = []
+    columns = ', '.join(columns)
+
+    # Retrieve the latest 200 records, ordered by date in descending order  
+    # to prioritize the most recent entries when checking for duplicates.
+    QUERY = (
+        f'SELECT {columns} FROM `{table_id}` '
+        'ORDER BY date DESC '
+        'LIMIT 200'
+    )
+    query_job = client.query(QUERY) 
+    rows = query_job.result() 
+
+    for row in rows:
+        unique_keys.append(dict(row))
+    return unique_keys
+
+def updateCompany(tickers):
+    """Update company info, unnecessary to run daily unless its for a refresh of market cap"""
+    rows_to_insert = []
 
     for ticker in tickers:
-        prices_rows = []
-
         dat = yf.Ticker(ticker)
         info = dat.info
-        prices = dat.history(period = '10y').reset_index()
+
+        rows_to_insert.append({
+            'stockTicker': ticker,
+            'city': info['city'], 
+            'industry': info['industry'], 
+            'marketCap': info['marketCap'], 
+            'name': info['shortName'],
+            'sector': info['sector'], 
+            'state': info['state']})
+
+    writeData(dataset_id + 'Company', rows_to_insert)
+
+def update_historical_prices(tickers):
+
+    for ticker in tickers:
+        rows_to_insert = []
+        dat = yf.Ticker(ticker)
+
+        # Limit data to 5 days for performance
+        prices = dat.history(period = '5d').reset_index()
         prices[['Date', 'Trading Period']] = prices['Date'].astype(str).str.split(' ', expand = True)
-
-
-        company_rows.append({'stockTicker': ticker, 'city': info['city'], 
-              'industry': info['industry'], 'marketCap': info['marketCap'], 
-              'name': info['shortName'], 'sector': info['sector'], 'state': info['state']})
         
+        existing_keys = readData(dataset_id + 'Historical Prices', ['date', 'stockTicker'])
+
         for _, row in prices.iterrows():
-            prices_rows.append({
-                'date': row['Date'],
-                'open': row['Open'],
-                'high': row['High'],
-                'low': row['Low'],
-                'close': row['Close'],
-                'volume': row['Volume'],
-                'dividends': row['Dividends'],
-                'stockSplits': row['Stock Splits'],
-                'tradingPeriod': row['Trading Period'],
-                'stockTicker': ticker
-            })
-        
-        
-    # readData(dataset_id + 'Company', company_rows)
-        readData(dataset_id + 'Historical Prices', prices_rows)
+            primary_key = {'date': pd.to_datetime(row['Date']).date(), 'stockTicker': ticker}
+            if primary_key not in existing_keys:
+
+                rows_to_insert.append({
+                    'date': row['Date'],
+                    'open': row['Open'],
+                    'high': row['High'],
+                    'low': row['Low'],
+                    'close': row['Close'],
+                    'volume': row['Volume'],
+                    'dividends': row['Dividends'],
+                    'stockSplits': row['Stock Splits'],
+                    'tradingPeriod': row['Trading Period'],
+                    'stockTicker': ticker
+                })
+
+        if len(rows_to_insert) == 0:
+            print(f'No rows to insert, data is up-to-date for {ticker}.')
+        else:    
+            writeData(dataset_id + 'Historical Prices', rows_to_insert)
+            print(f'{len(rows_to_insert)} rows updated.')
 
 
 if __name__ == '__main__':
-    update(tickers)
+    # update_historical_prices(tickers)
+    print(readData(dataset_id + 'Historical Prices', ['date', 'stockTicker']))
 
